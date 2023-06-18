411ba2cb6788a7468447ce8ea3dce366139a4b6b
André Vargas Roo <a.vargas252@icloud.com>
2023-06-18 18:28:39 -0400

    bot is live

--- orzabal.py
+++ orzabal.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 import asyncio
+from secret import bot_secret
 
 orzabal = commands.Bot(command_prefix='!')
+
+@orzabal.event
+async def on_ready():
+    print(f'Orzabal is ready! Logged in as {orzabal.user.name}')
+
+orzabal.run(bot_secret)
 
