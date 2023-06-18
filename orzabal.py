27b5324bc65efa2c62e3cb3f21e5edc8986bdceb
André Vargas Roo <a.vargas252@icloud.com>
2023-06-18 18:52:51 -0400

    initialize

--- orzabal.py
+++ orzabal.py
@@ -1,14 +1,14 @@
 import discord
 from secret import bot_secret
 
-while True:
-    intents = discord.Intents.default()
-    intents.message_content = True
+intents = discord.Intents.default()
+intents.message_content = True
 
-    client = discord.Client(intents=intents)
+client = discord.Client(intents=intents)
 
-    @client.event
-    async def on_ready():
-        print(f'We have logged in as {client.user}')
+@client.event
+async def on_ready():
+    print(f'We have logged in as {client.user}')
 
-    client.run(bot_secret)
+
+client.run(bot_secret)
