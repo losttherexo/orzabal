a3f85a5f2b2c640854bb77501da2fca66e7a08e3
André Vargas Roo <a.vargas252@icloud.com>
2023-06-19 10:23:37 -0400

    changed from discord client to bot

--- orzabal.py
+++ orzabal.py
@@ -3,6 +3,7 @@ import discord
 import datetime
 import asyncio
 from dotenv import load_dotenv
+from discord.ext import commands
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -11,32 +12,44 @@ GUILD = os.getenv('DISCORD_GUILD')
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
-    
-    print(f'{client.user} has entered {guild.name}. You may now commence the mayhem.')
+    guild = discord.utils.get(bot.guilds, name=GUILD)
+    print(f'{bot.user} has entered {guild.name}. You may now commence the mayhem.')
     
     await morning_message()
 
+@bot.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hello {member.name}, welcome to the lab!')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    if bot.user.mentioned_in(message):
+        await message.channel.send("Hello! I'm a Discord bot.")
+
+    await bot.process_commands(message)
+
 async def send_morning_message():
     channel_id = 748224867085582425
-    channel = client.get_channel(channel_id)
+    channel = bot.get_channel(channel_id)
     if channel:
         await channel.send("Good morning, everyone! Let's rule the fucking world")
 
 async def morning_message():
     while True:
         now = datetime.datetime.now()
-        target_time = datetime.time(hour=9, minute=00)  # Set the target time for the message
+        target_time = datetime.time(hour=9, minute=00)
         target_datetime = datetime.datetime.combine(now.date(), target_time)
 
         if now.time() > target_time:
-            target_datetime += datetime.timedelta(days=1)  # Add 1 day if the target time has already passed today
+            target_datetime += datetime.timedelta(days=1)
 
         delta = target_datetime - now
         seconds_until_target = delta.total_seconds()
@@ -45,12 +58,4 @@ async def morning_message():
 
         await send_morning_message()
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-    if message.content == 'hello':
-        await message.author.send('hello my friend!')
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
