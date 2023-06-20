6807b275e8511f307da44e38907f7a7577455312
André Vargas Roo <a.vargas252@icloud.com>
2023-06-19 22:31:52 -0400

    troubleshooting morning and night messages

--- orzabal.py
+++ orzabal.py
@@ -1,9 +1,8 @@
 import os
 import discord
 import datetime
-import asyncio
 from dotenv import load_dotenv
-from discord.ext import commands
+from discord.ext import commands,tasks
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -14,13 +13,14 @@ intents.message_content = True
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
+morning_time = datetime.time(7, 45, 0)
+night_time = datetime.time(22, 45, 0)
+
 @bot.event
 async def on_ready():
     guild = discord.utils.get(bot.guilds, name=GUILD)
     print(f'{bot.user} has entered {guild.name}. You may now commence the mayhem.')
-    
-    await morning_message()
-    await goodnight_message()
+    send_daily_messages.start()
 
 @bot.event
 async def on_member_join(member):
@@ -37,48 +37,18 @@ async def on_message(message):
 
     await bot.process_commands(message)
 
-async def send_morning_message():
-    channel_id = 748224867085582425
-    channel = bot.get_channel(channel_id)
-    if channel:
-        await channel.send("Good morning, everyone! Let's rule the fucking world.")
+@tasks.loop(hours=24)
+async def send_daily_messages():
+    now = datetime.datetime.now().time()
+    print(f"Current time: {now}")
 
-async def morning_message():
-    while True:
-        now = datetime.datetime.now()
-        target_time = datetime.time(hour=7, minute=45)
-        target_datetime = datetime.datetime.combine(now.date(), target_time)
+    if now == morning_time:
+        channel = bot.get_channel(748224867085582425)
+        await channel.send("Good morning yall!")
 
-        if now.time() > target_time:
-            target_datetime += datetime.timedelta(days=1)
+    if now == night_time:
+        channel = bot.get_channel(748224867085582425)
+        await channel.send("Don't forget to drink water before bed tonight <3")
 
-        delta = target_datetime - now
-        seconds_until_target = delta.total_seconds()
-
-        await asyncio.sleep(seconds_until_target)
-
-        await send_morning_message()
-
-async def send_goodnight_message():
-    channel_id = 748224867085582425
-    channel = bot.get_channel(channel_id)
-    if channel:
-        await channel.send("Make sure you drink some water and get a good night's sleep yall!")
-
-async def goodnight_message():
-    while True:
-        now = datetime.datetime.now()
-        target_time = datetime.time(hour=1, minute=58)
-        target_datetime = datetime.datetime.combine(now.date(), target_time)
-
-        if now.time() > target_time:
-            target_datetime += datetime.timedelta(days=1)
-
-        delta = target_datetime - now
-        seconds_until_target = delta.total_seconds()
-
-        await asyncio.sleep(seconds_until_target)
-
-        await send_goodnight_message()
 
 bot.run(TOKEN)
